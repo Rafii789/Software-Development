f6f263f1587c59ebd5816f6397de39b9411bbda0
Rafii789 <sayedhossainsk@gmail.com>
2024-12-09 14:38:41 +0600

    update

--- Connecting To Database/routes/web.php
+++ Connecting To Database/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\DB;
+Route::get('/test-database', function () {
+// Fetch data from the 'users' table
+$users = DB::select('SELECT * FROM users');
+return $users; // Show the users data (empty array if no data)
+});
